29b7ecfc60d9bdaceef61605fed8ea1bd846baef
Danilo Yorinori <daniloksy@gmail.com>
2024-11-08 19:35:52 -0300

    Use destination instead of env variables

--- src/sales-order/sales-order.service.ts
+++ src/sales-order/sales-order.service.ts
@@ -3,20 +3,32 @@ import {
   apiSalesOrderSrv,
   SalesOrder,
 } from '../../services/API_SALES_ORDER_SRV';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SalesOrdersService {
+  constructor(private configService: ConfigService) {}
+
   async getAllSalesOrders(): Promise<SalesOrder[]> {
     const { salesOrderApi } = apiSalesOrderSrv();
-    return await salesOrderApi.requestBuilder().getAll().top(5).execute({
-      destinationName: 'MockServer',
-    });
+    return await salesOrderApi
+      .requestBuilder()
+      .getAll()
+      .top(5)
+      .addCustomHeaders({ APIKey: this.configService.get<string>('API_KEY') })
+      .execute({
+        destinationName: this.configService.get<string>('DESTINATION_NAME'),
+      });
   }
 
   async getSalesOrdersItem(id: string): Promise<SalesOrder> {
     const { salesOrderApi } = apiSalesOrderSrv();
-    return await salesOrderApi.requestBuilder().getByKey(id).execute({
-      destinationName: 'MockServer',
-    });
+    return await salesOrderApi
+      .requestBuilder()
+      .getByKey(id)
+      .addCustomHeaders({ APIKey: this.configService.get<string>('API_KEY') })
+      .execute({
+        destinationName: this.configService.get<string>('DESTINATION_NAME'),
+      });
   }
 }
